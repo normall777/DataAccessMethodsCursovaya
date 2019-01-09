c790d0c4bc5204024f2f3c949c1665bc0e740479
Rafnsvart <rafnsvart.d@yandex.ru>
2019-01-09 23:47:11 +0500

    213

---
+++ ln.R
@@ -0,0 +1,11 @@
+fun.ln<-function()
+{
+  data <- read.csv("kc_house_data.csv", header=TRUE, sep=",");
+  data <- data[,-c(1,2)];
+  data <- data[data$sqft_living<13000,];
+  data<- data[data$price<2*10^6,];
+  data$avgZipCodePrice <- ave(data$price,data$zipcode);
+  lm.data.in <- lm(price~sqft_living+yr_built+grade+waterfront+avgZipCodePrice, data = data);
+  return (c(lm.data.in, data$zipcode));
+}
+
--- rscript.R
+++ rscript.R
@@ -1,14 +1,15 @@
-my.f<-function(sqft_living, view, yr_built, grade, lat, waterfront){
+my.f<-function(sqft_living, yr_built, grade,  waterfront, zipcode){
   data <- read.csv("kc_house_data.csv", header=TRUE, sep=",");
   data <- data[,-c(1,2)];
   data <- data[data$sqft_living<13000,];
   data<- data[data$price<2*10^6,];
-  lm.data.in <- lm(price~sqft_living+view+yr_built+grade+lat+waterfront, data = data)
-  #data.predict.out <- predict(lm.data.in, c(sqft_living, view, yr_built, grade, lat))
-  ret <- coef(lm.data.in)[1] + coef(lm.data.in)[2]*sqft_living + coef(lm.data.in)[3]*view +
-    coef(lm.data.in)[4]*yr_built + coef(lm.data.in)[5]*grade + coef(lm.data.in)[6]*lat + 
-    coef(lm.data.in)[7]*waterfront;
+  data$avgZipCodePrice <- ave(data$price,data$zipcode)
+  avg <-mean(data[data$zipcode==zipcode,]$price)
+  lm.data.in <- lm(price~sqft_living+yr_built+grade+waterfront+avgZipCodePrice, data = data)
+  ret <- coef(lm.data.in)[1] + coef(lm.data.in)[2]*sqft_living +
+    coef(lm.data.in)[3]*yr_built + coef(lm.data.in)[4]*grade  + 
+    coef(lm.data.in)[5]*waterfront + coef(lm.data.in)[6]*avg;
   return (as.numeric(ret))
 }
-my.f(2570, 0, 1951, 7, 47.721, 0)
+#my.f(2570, 1951, 7, 0, 98125)
 
