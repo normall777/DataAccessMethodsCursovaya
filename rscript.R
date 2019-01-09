94e2728056b77c89b58314304c03f17383c98cee
Rafnsvart <rafnsvart.d@yandex.ru>
2019-01-09 20:33:52 +0500

    waterfront

--- rscript.R
+++ rscript.R
@@ -1,12 +1,14 @@
-my.f<-function(sqft_living, view, yr_built, grade, lat){
-  data <- read.csv("kc_house_data.csv", header=TRUE, sep=",")
-  data <- data[,-c(1,2)]
-  data <- data[data$sqft_living<13000,]
-  data<- data[data$price<2*10^6,]
-  lm.data.in <- lm(price~sqft_living+view+yr_built+grade+lat, data = data)
+my.f<-function(sqft_living, view, yr_built, grade, lat, waterfront){
+  data <- read.csv("kc_house_data.csv", header=TRUE, sep=",");
+  data <- data[,-c(1,2)];
+  data <- data[data$sqft_living<13000,];
+  data<- data[data$price<2*10^6,];
+  lm.data.in <- lm(price~sqft_living+view+yr_built+grade+lat+waterfront, data = data)
   #data.predict.out <- predict(lm.data.in, c(sqft_living, view, yr_built, grade, lat))
-  ret <- coef(lm.data.in)[1] + coef(lm.data.in)[2]*sqft_living + coef(lm.data.in)[3]*view +coef(lm.data.in)[4]*yr_built + coef(lm.data.in)[5]*grade + coef(lm.data.in)[6]*lat
+  ret <- coef(lm.data.in)[1] + coef(lm.data.in)[2]*sqft_living + coef(lm.data.in)[3]*view +
+    coef(lm.data.in)[4]*yr_built + coef(lm.data.in)[5]*grade + coef(lm.data.in)[6]*lat + 
+    coef(lm.data.in)[7]*waterfront;
   return (as.numeric(ret))
 }
-my.f(2570, 0, 1951, 7, 47.721)
+my.f(2570, 0, 1951, 7, 47.721, 0)
 
