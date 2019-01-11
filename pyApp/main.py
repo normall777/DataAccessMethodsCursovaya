d3bfa0ba4854f6d33747a21d3c0800d09ca05ae1
Ilya Izotov <normall777@yandex.ru>
2019-01-11 17:49:01 +0500

    Добавлены комментарии к приложению

--- pyApp/main.py
+++ pyApp/main.py
@@ -4,10 +4,12 @@ import  design
 import numpy as np
 import rpy2.robjects as robjects
 
-class pyApp(QtWidgets.QMainWindow, design.Ui_MainWindow, QtWidgets.QDialogButtonBox):
+#Класс UI приложения
+class pyApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        #Получение списка индексов округа и приведение его к нужному типу
         list_r = robjects.r(f"fun.zip()")
         list_r = np.asarray(list_r)
         list_r = np.array(map(str, list_r))
@@ -15,11 +17,13 @@ class pyApp(QtWidgets.QMainWindow, design.Ui_MainWindow, QtWidgets.QDialogButton
         list_r = list(list_r)
         list_r.sort()
         self.list_r = list_r
-        self.listZip.addItems(self.list_r)
-        self.pushButtonPredict.clicked.connect(self.Predict)
+        self.listZip.addItems(self.list_r) #Передача индексов в интерфейс для возможности дальнейшего выбора
         self.listZip.setCurrentRow(0)
+        self.pushButtonPredict.clicked.connect(self.Predict) #Присоединение к кнопке функции
 
+    #Функция возвращает предсказанное значение
     def Predict(self):
+        #Получение параметров из формы интерфейса
         sqftLiving = self.spinBoxSqftLiving.value()
         yrBuilt = self.spinBoxYrBuilt.value()
         grade = self.spinBoxGrade.value()
@@ -30,9 +34,12 @@ class pyApp(QtWidgets.QMainWindow, design.Ui_MainWindow, QtWidgets.QDialogButton
         elif self.radioButtonNo.isChecked():
             waterfront = 0
 
+        #Получение предсказанного значения
         predictFromR = robjects.r(f"fun.predict({sqftLiving}, {yrBuilt}, {grade}, {waterfront}, {zipcode})")
         predictFromR = int(predictFromR[0])
+        #Проверка предсказанного числа на корректность и его вывод экран
         if predictFromR < 0:
+            #В случае ошибки возникает окно с сообщением об ошибке
             self.lcdNumberResult.setProperty("intValue", 0)
             msgBox = QtWidgets.QMessageBox()
             msgBox.setText('Ошибка предсказания')
@@ -41,6 +48,7 @@ class pyApp(QtWidgets.QMainWindow, design.Ui_MainWindow, QtWidgets.QDialogButton
         else:
             self.lcdNumberResult.setProperty("intValue", predictFromR)
 
+#Инициализация R и UI
 def main():
     robjects.r("source('R.R')")
     app = QtWidgets.QApplication(sys.argv)
@@ -48,5 +56,6 @@ def main():
     window.show()
     app.exec_()
 
+#Точка старта
 if __name__ == '__main__':
     main()
